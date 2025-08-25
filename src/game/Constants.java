37299da3a66fd5792972ee6e383a01562dc437fe
Marwan <marwanelgharbawy2@gmail.com>
2025-08-26 01:51:55 +0300

    Initial Player implementation

---
+++ src/game/Constants.java
@@ -0,0 +1,6 @@
+package game;
+
+public interface Constants {
+    int DefaultHealth = 100;
+    int DefaultBalance = 10;
+}
--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,22 @@
 package game;
 
+import java.util.ArrayList;
+import units.Unit;
+
 public class Player {
     // Available units, main stats such as health (that gets updated after each round)
+    // For current design, each player has no special abilities
+    // For current design, each player has a fixed set of units they can use
+    // TODO: Later, this will become an abstract class which special heroes will extend
+    private String name;
+    private int health;
+    private int balance;
+    private ArrayList<Unit> units; // List of units owned by the player
+
+    public Player(String name) {
+        this.name = name;
+        this.health = Constants.DefaultHealth;   // Default health
+        this.balance = Constants.DefaultBalance; // Default balance
+        this.units = new ArrayList<>(); // Initialize the list of units
+    }
 }
