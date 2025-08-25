fe00c41adaca635a6bf5ca9fb8ca9ce288ce335d
Marwan <marwanelgharbawy2@gmail.com>
2025-08-26 02:54:09 +0300

    Separate methods into setup and game

--- src/game/GameEngine.java
+++ src/game/GameEngine.java
@@ -18,6 +18,7 @@ public class GameEngine {
         this.board = new Board(); // Initialize the board
     }
 
+    // Board setup methods
     public void placeUnit(int row, int col, Unit unit) {
             board.placeUnitAt(row, col, unit);
     }
@@ -28,5 +29,31 @@ public class GameEngine {
 
     public void deleteUnit(int row, int col) {
         board.placeUnitAt(row, col, null); // Remove unit by setting to null
+    }
+
+    // Game logic methods
+    public void startGame() {
+        // TODO: Game logic, including unit movement (forward) and attacking
+    }
+
+    public void unitAction(Unit unit) {
+        // Attack if enemy in range, else move forward
+    }
+
+    public void moveUnit(Unit unit) {
+        // Simply move forward one cell if it's empty
+    }
+
+    public void unitAttack(Unit attacker, Unit target) {
+        attacker.dealDamage(target);
+    }
+
+    public void killUnit(Unit unit) {
+        // unit.die(); ?
+    }
+
+    public void endGame() {
+        // Reset the board
+        // Score calculation
     }
 }
