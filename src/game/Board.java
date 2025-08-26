b85d3ffcef8e3c6c2b69054334b68b821d2acf60
Marwan <marwanelgharbawy2@gmail.com>
2025-08-26 19:15:48 +0300

    Add copy constructor

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,25 +1,51 @@
 package game;
-import units.Unit;
+
+import units.*;
 
 public class Board {
-    // Board of 8 rows and 5 columns (4 rows for each player)
-    // All cells are null initially
-    private Unit[][] board = new Unit[8][5];
+    private Unit[][] grid;
 
-    // Empty constructor
     public Board() {
+        // Board of 8 rows and 5 columns (4 rows for each player)
+        // All cells are null initially
+        this.grid = new Unit[8][5];
+    }
 
+    // Copy constructor
+    public Board(Board source) {
+        this.grid = new Unit[8][5];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                this.grid[i][j] = source.grid[i][j].copy(); // Deep copy each unit
+            }
+        }
     }
 
     public Unit getUnitAt(int row, int col) {
-        return board[row][col];
+        return grid[row][col];
     }
 
     public void placeUnitAt(int row, int col, Unit unit) {
-        if (board[row][col] == null) {
-            board[row][col] = unit;
+        if (grid[row][col] == null) {
+            grid[row][col] = unit;
         } else {
             throw new IllegalArgumentException("Cell is already occupied");
         }
+    }
+
+    public void removeUnit(Unit unit) {
+        // Search for unit and remove it
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == unit) {
+                    grid[i][j] = null;
+                    return;
+                }
+            }
+        }
+    }
+
+    public void removeUnitAt(int row, int col) {
+        grid[row][col] = null;
     }
 }
